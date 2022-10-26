d9392afe9c16a619daf8554afd838a1afbc1f7ec
DJun <32353521+aiyojun@users.noreply.github.com>
2022-10-26 17:20:13 +0800

    add two python micro services
    1. the standard file transport service
    2. user authentication micro service

---
+++ pyuas/service_ftp.py
@@ -0,0 +1,141 @@
+import os
+import logging
+import argparse
+from urllib.parse import unquote
+
+from tornado.web import Application, RequestHandler
+from tornado.ioloop import IOLoop
+from tornado.httpserver import HTTPServer
+
+
+def arg_help():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', default=8000, help='port of service')
+    parser.add_argument('-D', '--directory', default=".", help='directory of static files')
+    parser.add_argument('-f', '--prefix', default="", help='prefix of url')
+    return parser.parse_args()
+
+
+logging.basicConfig(
+    format='%(asctime)s [%(threadName)s] [%(name)s] [%(levelname)s] %(filename)s[line:%(lineno)d] %(message)s',
+    level=logging.INFO
+)
+args = arg_help()
+static_path = args.directory
+service_prefix = args.prefix
+
+
+class FileManagerHandler(RequestHandler):
+    @staticmethod
+    def trim(path):
+        path = "/".join(list(filter(lambda s: len(s) != 0, path.split('/'))))
+        return path
+
+    @staticmethod
+    def real_file_name(path):
+        arr = list(filter(lambda s: len(s) != 0, path.split('/')))
+        return arr[-1]
+
+    @staticmethod
+    def list_dir(path):
+        global static_path
+        real_path = static_path + "/" + FileManagerHandler.trim(path)
+        if real_path[-1] != "/":
+            real_path += "/"
+        files = os.listdir(real_path)
+        ul = ""
+        html = '''<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>Directory listing for ${PATH}$</title></head><body><h1>Directory listing for ${PATH}$</h1><hr><ul>${CONTENT}$</ul><hr></body></html>'''
+        for file in files:
+            if os.path.isdir(real_path + file):
+                real_dir = file + "/"
+                ul += "<li><a href=\"%s\">%s</a></li>" % (real_dir, real_dir)
+            else:
+                ul += "<li><a href=\"%s\">%s</a></li>" % (file, file)
+        html = html.replace("${PATH}$", real_path.replace(static_path, ""))
+        html = html.replace("${CONTENT}$", ul)
+        return html
+
+    @staticmethod
+    def get_file_path(path):
+        global static_path
+        return static_path + "/" + FileManagerHandler.trim(path)
+
+    def get(self, *arg, **kwargs):
+        relative_path = unquote(self.request.path)
+        global service_prefix
+        if relative_path.find(service_prefix) >= 0:
+            relative_path = relative_path[len(service_prefix):]
+        if len(relative_path) == 0:
+            relative_path = "/"
+        if relative_path[-1] == "/":
+            self.write(FileManagerHandler.list_dir(relative_path))
+        else:
+            real_file_path = FileManagerHandler.get_file_path(relative_path)
+            bytes_n = os.stat(real_file_path).st_size
+            self.set_header("Content-Description", "File Transfer")
+            self.set_header("Content-Type", "application/octet-stream")
+            self.set_header("Content-Length", str(bytes_n))
+            self.set_header("Content-Disposition", FileManagerHandler.real_file_name(real_file_path))
+            self.set_header("Content-Transfer-Encoding", "binary")
+            with open(real_file_path, 'rb+') as fp:
+                line = fp.readline()
+                while line:
+                    self.write(line)
+                    line = fp.readline()
+
+    def post(self, *arg, **kwargs):
+        relative_path = unquote(self.request.path)
+        global service_prefix
+        if relative_path.find(service_prefix) >= 0:
+            relative_path = relative_path[len(service_prefix):]
+        # form_keys = list(self.request.files.keys())
+        # for form_key in form_keys:
+        #     form = self.request.files.get(form_key)
+        multi_files = self.request.files.get("filepond")
+        if not multi_files:
+            self.write({"code": 500})
+            return
+        for file in multi_files:
+            filename = file["filename"]
+            real_dir = FileManagerHandler.trim(static_path + "/" + relative_path) + "/"
+            if not os.path.exists(real_dir):
+                os.mkdir(real_dir)
+            real_file_path = FileManagerHandler.trim(real_dir + "/" + filename)
+            if os.path.exists(real_file_path):
+                os.remove(real_file_path)
+            with open(real_file_path, 'wb+') as fp:
+                fp.write(file["body"])
+        self.write({"code": 200})
+
+    def delete(self, *arg, **kwargs):
+        relative_path = unquote(self.request.path)
+
+
+class Service:
+    _port = 8000
+    _service_name = "general service"
+    _version = "v0.1"
+
+    def __init__(self, **kwargs):
+        self._port = kwargs['port'] if 'port' in kwargs else self._port
+        self._service_name = kwargs['service_name'] if 'service_name' in kwargs else self._service_name
+        self._version = kwargs['version'] if 'version' in kwargs else self._version
+
+    def start(self, urls: list):
+        logging.info("%s started, listen on %d" % (self._service_name, self._port))
+        logging.info("Access prefix    : %s" % service_prefix)
+        logging.info("Access directory : %s" % args.directory)
+        HTTPServer(Application(urls)).listen(self._port)
+        IOLoop.current().start()
+
+
+def main():
+    urls = [
+        (r'^/(.*?)$', FileManagerHandler),
+    ]
+    http = Service(port=int(args.port), service_name="FTP Service", version="v0.1")
+    http.start(urls)
+
+
+if __name__ == '__main__':
+    main()
---
+++ pyuas/service_uas.py
@@ -0,0 +1,228 @@
+import os
+import json
+import logging
+import argparse
+
+from tornado.web import Application, RequestHandler
+from tornado.ioloop import IOLoop
+from tornado.httpserver import HTTPServer
+
+import hashlib
+import uuid as uuid_pack
+from pymongo import MongoClient
+
+
+def arg_help():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', default=8000, help='port of service')
+    parser.add_argument('-s', '--services', default="services.json", help='services mapping')
+    parser.add_argument('-M', '--mongodb', default="127.0.0.1:27017", help='mongodb connection')
+    return parser.parse_args()
+
+
+logging.basicConfig(
+    format='%(asctime)s [%(threadName)s] [%(name)s] [%(levelname)s] %(filename)s[line:%(lineno)d] %(message)s',
+    level=logging.INFO
+)
+args = arg_help()
+
+
+UAS_SIGN = 'uas'
+UAS_USERS = 'users'
+UAS_LOGIN = 'login_records'
+
+
+def md5(s):
+    return hashlib.md5(s.encode(encoding='utf-8')).hexdigest()
+
+
+def uuid():
+    return str(uuid_pack.uuid1()).replace("-", "")
+
+
+class UserManager:
+    data_source = None
+    login_records = None
+
+    def __init__(self, mongodb):
+        self.data_source = MongoClient(mongodb)[UAS_SIGN][UAS_USERS]
+        self.login_records = MongoClient(mongodb)[UAS_SIGN][UAS_LOGIN]
+
+    def add_user(self, user):
+        if "username" not in user:
+            raise Exception("Lack of username.")
+        if "password" not in user:
+            raise Exception("Lack of password.")
+        user["password"] = md5(user["password"])
+        user_token = uuid()
+        user["id"] = user_token
+        self.data_source.insert_one(user)
+        return user_token
+
+    def register_user_by_email(self, email, password):
+        return self.add_user({"username": "", "password": password, "email": email})
+
+    def modify_password_by_email(self, email, new_password):
+        cursor = self.data_source.find({"email": email}, {"_id": 0})
+        users = []
+        for every in cursor:
+            users.append(every)
+        if len(users) == 0:
+            return
+        users[0]['password'] = new_password
+
+    def find_by_id(self, user_token):
+        cursor = self.data_source.find({"id": user_token}, {"_id": 0})
+        rs = []
+        for every in cursor:
+            rs.append(every)
+        if len(rs) == 0:
+            return None
+        return rs[0]
+
+    def find_by_name(self, username):
+        users = []
+        cursor = self.data_source.find({"username": username}, {"_id": 0})
+        for every in cursor:
+            users.append(every)
+        return users
+
+    def find_by_email(self, email):
+        cursor = self.data_source.find({"email": email}, {"_id": 0})
+        users = []
+        for every in cursor:
+            users.append(every)
+        return users[0] if len(users) != 0 else None
+
+    def validate(self, token, service_id):
+        record = []
+        cursor = self.login_records.find({"token": token}, {"_id": 0})
+        for every in cursor:
+            record.append(every)
+        if len(record) == 0:
+            raise Exception("No such token!")
+        user = []
+        cursor = self.data_source.find({"email": record[0]["email"]}, {"_id": 0})
+        for every in cursor:
+            user.append(every)
+        if len(user) == 0:
+            raise Exception("No such user whose email : %s" % record[0]['email'])
+        user = user[0]
+        if "permissions" not in user:
+            raise Exception("No extra permissions!")
+        if service_id not in user["permissions"]:
+            raise Exception("No [%s] service permissions!" % service_id)
+
+    def do_authentication(self, email, password):
+        user = self.find_by_email(email)
+        if user is None:
+            raise Exception("Can't find user of %s!" % email)
+        if md5(password) == user["password"]:
+            token = uuid()
+            self.login_records.insert_one({"email": email, "token": token})
+            return token
+        raise Exception("Error password!")
+
+    def get_all_users(self):
+        cursor = self.data_source.find({}, {"_id": 0})
+        users = []
+        for every in cursor:
+            users.append(every)
+        return users
+
+    def remove_user_by_username(self, username):
+        self.data_source.delete_many({"username": username})
+
+    def remove_all_users(self):
+        self.data_source.delete_many({})
+
+
+userManager = UserManager(args.mongodb)
+limited_prefix = {}
+
+
+class UserLoginHandler(RequestHandler):
+    def post(self):
+        req = json.loads(self.request.body.decode(encoding='UTF-8'))
+        try:
+            token = userManager.do_authentication(req["username"], req["password"])
+            self.write({
+                "code": 200,
+                "token": token
+            })
+        except Exception as e:
+            self.write({
+                "code": 400,
+                "error": str(e)
+            })
+
+
+class GateHandler(RequestHandler):
+    @staticmethod
+    def split_path(path):
+        return list(filter(lambda x: len(x) != 0, path.split('/')))
+
+    def proxy(self, status=302):
+        prefix = GateHandler.split_path(self.request.path)[0]
+        global limited_prefix
+        if prefix in limited_prefix:
+            token = self.get_query_argument("id")
+            try:
+                userManager.validate(token, limited_prefix.get(prefix).get("service_id"))
+            except Exception as e:
+                self.write({
+                    "code": 401,
+                    "error": "Authentication error, %s" % str(e),
+                })
+                self.finish()
+                return
+            self.redirect("http://%s%s" % (limited_prefix.get(prefix).get("target"), self.request.uri), status=status)
+        else:
+            self.write({"code": 404, "error": "Unknown service!"})
+            self.finish()
+
+    def get(self, *arg, **kwargs):
+        self.proxy()
+
+    def post(self, *arg, **kwargs):
+        self.proxy(307)
+
+
+class Service:
+    _port = 8000
+    _service_name = "general service"
+    _version = "v0.1"
+
+    def __init__(self, **kwargs):
+        self._port = kwargs['port'] if 'port' in kwargs else self._port
+        self._service_name = kwargs['service_name'] if 'service_name' in kwargs else self._service_name
+        self._version = kwargs['version'] if 'version' in kwargs else self._version
+
+    def start(self, urls: list):
+        logging.info("%s started, listen on %d" % (self._service_name, self._port))
+        HTTPServer(Application(urls)).listen(self._port)
+        IOLoop.current().start()
+
+
+def main():
+    global limited_prefix
+    if os.path.isfile(args.services):
+        with open(args.services, 'r+') as fp:
+            js = json.loads(fp.read())
+            for service_id, service_info in js.items():
+                limited_prefix[service_info["prefix"]] = {
+                    "service_id": service_id,
+                    "target": service_info["target"],
+                }
+    logging.info("Loading services : " + json.dumps(limited_prefix))
+    logging.info("Mongodb services : %s" % args.mongodb)
+    urls = [
+        (r"/login", UserLoginHandler),
+        (r'^/(.*?)$', GateHandler),
+    ]
+    http = Service(port=int(args.port), service_name="UAS Service", version="v0.1")
+    http.start(urls)
+
+
+if __name__ == '__main__':
+    main()
